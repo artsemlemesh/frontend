a652a7deaca58db3ef218145e219200e6e847f29
artsemlemesh <126878429+artsemlemesh@users.noreply.github.com>
2024-11-11 08:36:06 +0800

    tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-
-import './App.css'
+// import './index.css'
 
 function App() {
 
   return (
     <>
-    <h1>Hello World</h1>
+    <div className="flex items-center justify-center min-h-screen bg-blue-100">
+      <h1 className="text-4xl font-bold text-blue-600">Hello, Tailwind CSS!</h1>
+    </div>
     </>
   )
 }
